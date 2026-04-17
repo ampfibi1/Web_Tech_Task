2a2217fa398bca786f52462ad0aee9a364b0d7f2
Abdullah Al-Tamjid <168060724+ampfibi1@users.noreply.github.com>
2026-04-17 22:55:54 +0600

    version_2.0

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -4,6 +4,13 @@ function operation() {
     const heightFeet = document.getElementById("height_feet").value;
     const heightInch = document.getElementById("height_inch").value;
 
+    const genderInput = document.querySelector('input[name="gender"]:checked');
+    const gender = genderInput ? genderInput.value : null;
+
+    const age = document.getElementById("age").value;
+
+    let bmr_res= document.getElementById("bmr_result");
+
     let heightInMeters = (heightFeet * 0.3048) + (heightInch * 0.0254);
 
     const weight = document.getElementById("weight_kg").value;
@@ -11,9 +18,25 @@ function operation() {
 
     let resultText = `Your BMI is ${bmi.toFixed(2)}. `;
 
-    if( !isNaN(bmi)) {
+    if( !isNaN(bmi) && bmi !== Infinity) {
         let res = document.getElementById("bmi_result");
         res.innerHTML = resultText;
+
+
+        if(gender === "male"){
+            let bmr = (10 * weight) + (6.25 * heightInMeters * 100) - (5 * age) + 5; // Assuming age is 30
+            bmr_res.innerHTML = `Your BMR is ${bmr.toFixed(2)} calories/day.`;
+            document.getElementById("bmr_fieldset").style.visibility = "visible";
+        }else if(gender === "female"){
+            let bmr = (10 * weight) + (6.25 * heightInMeters * 100) - (5 * age) - 161; // Assuming age is 30
+            bmr_res.innerHTML = `Your BMR is ${bmr.toFixed(2)} calories/day.`;
+            document.getElementById("bmr_fieldset").style.visibility = "visible";
+        }else{
+            bmr_res.innerHTML = "Please select a valid gender to calculate BMR.";
+            document.getElementById("bmr_fieldset").style.visibility = "visible";
+        }
+
+        
 
         if(bmi < 18.5) {
             res.innerHTML += "<br>You are underweight.";
@@ -31,5 +54,4 @@ function operation() {
     }
     
 }
-
 btncall.addEventListener("click", operation);
